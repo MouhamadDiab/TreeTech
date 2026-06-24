4f035055acdb3f9f2a366c82e3c04e471b05fcfc
MouhamadDiab <130409068+MouhamadDiab@users.noreply.github.com>
2026-06-24 23:50:57 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,22 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "treetech");
-if (!$conn) {
-    echo "data base is not connected";
+$url = getenv("MYSQL_URL");
+
+if ($url) {
+    $db = parse_url($url);
+
+    $host = $db["host"];
+    $user = $db["user"];
+    $pass = $db["pass"];
+    $port = $db["port"];
+    $name = ltrim($db["path"], "/");
+
+    $conn = mysqli_connect($host, $user, $pass, $name, $port);
+} else {
+    // Local XAMPP
+    $conn = mysqli_connect("127.0.0.1", "root", "", "treetech");
 }
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
